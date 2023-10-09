da4629a0b8eb58f84ecc8e0c308b243ab9a4cd3e
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-09 18:44:16 -0300

    key remapping

--- src/05-key-remapping/32-never-in-key-remapping.problem.ts
+++ src/05-key-remapping/32-never-in-key-remapping.problem.ts
@@ -1,25 +1,28 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 interface Example {
-  name: string;
-  age: number;
-  id: string;
-  organisationId: string;
-  groupId: string;
+	name: string
+	age: number
+	id: string
+	organisationId: string
+	groupId: string
 }
 
-type OnlyIdKeys<T> = unknown;
+type OnlyIdKeys<T> = { [K in keyof T as K extends `${string}${'id' | 'Id'}` ? K : never]: T[K] }
+
+type res = OnlyIdKeys<Example>
+//   ^?
 
 type tests = [
-  Expect<
-    Equal<
-      OnlyIdKeys<Example>,
-      {
-        id: string;
-        organisationId: string;
-        groupId: string;
-      }
-    >
-  >,
-  Expect<Equal<OnlyIdKeys<{}>, {}>>
-];
+	Expect<
+		Equal<
+			OnlyIdKeys<Example>,
+			{
+				id: string
+				organisationId: string
+				groupId: string
+			}
+		>
+	>,
+	Expect<Equal<OnlyIdKeys<{}>, {}>>
+]
--- src/05-key-remapping/36-discriminated-union-to-union.problem.ts
+++ src/05-key-remapping/36-discriminated-union-to-union.problem.ts
@@ -1,23 +1,20 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 type Fruit =
-  | {
-      name: "apple";
-      color: "red";
-    }
-  | {
-      name: "banana";
-      color: "yellow";
-    }
-  | {
-      name: "orange";
-      color: "orange";
-    };
+	| {
+			name: 'apple'
+			color: 'red'
+	  }
+	| {
+			name: 'banana'
+			color: 'yellow'
+	  }
+	| {
+			name: 'orange'
+			color: 'orange'
+	  }
 
-type TransformedFruit = unknown;
+type TransformedFruit = { [K in Fruit as K['name']]: `${K['name']}:${K['color']}` }[Fruit['name']]
+//      ^?
 
-type tests = [
-  Expect<
-    Equal<TransformedFruit, "apple:red" | "banana:yellow" | "orange:orange">
-  >,
-];
+type tests = [Expect<Equal<TransformedFruit, 'apple:red' | 'banana:yellow' | 'orange:orange'>>]

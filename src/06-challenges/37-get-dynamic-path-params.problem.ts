dfdc55737b890fe670b66bdabc9e9cd993bdb0d2
Lucas Coppola <112649792+LucasCoppola@users.noreply.github.com>
2023-10-12 18:34:24 -0300

    challenges

--- src/06-challenges/37-get-dynamic-path-params.problem.ts
+++ src/06-challenges/37-get-dynamic-path-params.problem.ts
@@ -1,17 +1,18 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { S } from 'ts-toolbelt'
+import { Equal, Expect } from '../helpers/type-utils'
 
-type UserPath = "/users/:id";
+type UserPath = '/users/:id'
 
-type UserOrganisationPath = "/users/:id/organisations/:organisationId";
+type UserOrganisationPath = '/users/:id/organisations/:organisationId'
 
-type ExtractPathParams = unknown;
+type ExtractPathParams<T extends string> = {
+	[I in S.Split<T, '/'>[number] as I extends `:${infer P}` ? P : never]: T[number]
+}
+
+type res = ExtractPathParams<UserOrganisationPath>
+// 	  ^?
 
 type tests = [
-  Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
-  Expect<
-    Equal<
-      ExtractPathParams<UserOrganisationPath>,
-      { id: string; organisationId: string }
-    >
-  >,
-];
+	Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
+	Expect<Equal<ExtractPathParams<UserOrganisationPath>, { id: string; organisationId: string }>>
+]
--- src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
+++ src/06-challenges/39-discriminated-union-with-unique-values-to-object.problem.ts
@@ -1,32 +1,32 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
 type Route =
-  | {
-      route: "/";
-      search: {
-        page: string;
-        perPage: string;
-      };
-    }
-  | { route: "/about" }
-  | { route: "/admin" }
-  | { route: "/admin/users" };
+	| {
+			route: '/'
+			search: {
+				page: string
+				perPage: string
+			}
+	  }
+	| { route: '/about' }
+	| { route: '/admin' }
+	| { route: '/admin/users' }
 
-type RoutesObject = unknown;
+type RoutesObject = { [R in Route as R['route']]: R extends { search: any } ? R['search'] : never }
 
 type tests = [
-  Expect<
-    Equal<
-      RoutesObject,
-      {
-        "/": {
-          page: string;
-          perPage: string;
-        };
-        "/about": never;
-        "/admin": never;
-        "/admin/users": never;
-      }
-    >
-  >,
-];
+	Expect<
+		Equal<
+			RoutesObject,
+			{
+				'/': {
+					page: string
+					perPage: string
+				}
+				'/about': never
+				'/admin': never
+				'/admin/users': never
+			}
+		>
+	>
+]
